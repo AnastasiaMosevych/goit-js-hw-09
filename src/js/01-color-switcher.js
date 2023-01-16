bf0cc07cf6ae9c0435ebad196ba28ae30949fc35
AnastasiaMosevych <ukrainischesmaedel@yahoo.de>
2023-01-17 00:44:54 +0100

    initial commit and completed task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+const startBtn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+const bodyEl = document.querySelector('body');
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+    timerId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  startBtn.disabled = false;
+  clearInterval(timerId);
+});
+
